2aa480163a558b90bc6041219a8a44ab14963d30
unkn3wn <reyesfernando151@gmail.com>
2022-12-01 13:04:54 -0800

    finishing up the hook for useUsers, done fetchUser, fetchRegister, fetchLogin

--- client/src/hooks/useUsers.js
+++ client/src/hooks/useUsers.js
@@ -3,10 +3,30 @@ import { useStoreState, useStoreActions } from "easy-peasy";
 export default function useUsers() {
   const users = useStoreState((state) => state.users.data);
   const fetchUsers = useStoreActions((actions) => actions.users.fetchUsers);
-  const register = useStoreActions((actions) => actions.users.register);
+
+  const user = useStoreState((state)=> state.user.data);
+  const fetchUser = useStoreActions((actions)=> actions.user.fetchUser)
+
+  const register = useStoreState((state)=> state.register.data);
+  const fetchRegister = useStoreActions((actions)=> actions.register.fetchUsers)
+
+  const login = useStoreState((state)=> state.login.data);
+  const fetchLogin = useStoreActions((actions) = actions.login.fetchLogin)
+
+  const me = useStoreState((state)=> actoins.me.data);
+  
 
   return {
     users,
     fetchUsers,
+
+    user, 
+    fetchUser,
+
+    register,
+    fetchRegister,
+
+    login, 
+    fetchLogin
   };
 }
