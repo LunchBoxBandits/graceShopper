9bc0efbb0e61f2c670a9cc653d6ba0915e7b5f29
Wilsonrchen <Wilsonrchen@gmail.com>
2022-12-02 19:26:42 -0800

    added shopping cart items, adding navbar functionality register moves to login on success, login on success moves to products page, logout moves to products and returns user to Guest

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,10 +1,46 @@
 import React from "react";
+import { useEffect } from "react";
+import useCart from "../hooks/useCart";
 
-export default function Cart(){
+export default function Cart() {
+  const { fetchCart, cart } = useCart();
 
-    return(
-        <h1>Hello</h1>
-    )
+  useEffect(() => {
+    fetchCart();
+  }, []);
+  console.log("cart in the Cart.jsx", cart);
+  return (
+    <>
+      <h1> Shopping Cart</h1>
+      <div>
+        {cart.order_products?.map((item) => {
+          console.log("inside the cart map:", cart.order_products);
+          console.log(" item inside the cart map:", item);
+          return (
+            <div>
+              <h2>{item.products.name}</h2>
+              <img width={50} height={50} src={item.products.imageUrl} />
+              <h3>${item.products.price}.00</h3>
+            </div>
+          );
+        })}
+      </div>
 
-
+      {/* <div>
+        {cart.map((item) => {
+          return (
+            <div>
+              {item.order_products.map((product) => {
+                return (
+                  <div>
+                    <h2>{product.name}</h2>{" "}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div> */}
+    </>
+  );
 }
--- client/src/hooks/useCart.js
+++ client/src/hooks/useCart.js
@@ -1,8 +1,10 @@
-import { useStoreActions } from "easy-peasy";
+import { useStoreState, useStoreActions } from "easy-peasy";
 
 export default function useCart() {
-  const fetchCart = useStoreActions((actions) => actions.users.fetchCart);
-  return{
-    fetchCart
-  }
+  const fetchCart = useStoreActions((actions) => actions.cart.fetchCart);
+  const cart = useStoreState((state) => state.cart.data);
+  return {
+    fetchCart,
+    cart,
+  };
 }
