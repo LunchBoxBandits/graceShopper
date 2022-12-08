6419b449f52c409ec235b6c544e0f20442c42f83
Wilsonrchen <Wilsonrchen@gmail.com>
2022-12-08 09:05:37 -0800

    edited home page to include a banner

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,7 +1,9 @@
 import styles from "../syles/Homepage.module.css";
 import { useNavigate } from "react-router-dom";
 import Button from "@mui/joy/Button";
-
+import wilson from "../assets/wilson pfp.jpeg";
+import ferni from "../assets/fernando pfp.png";
+import ugo from "../assets/ugonna pfp.png";
 function Home() {
   const nav = useNavigate();
   return (
@@ -13,39 +15,63 @@ function Home() {
       <div></div>
 
       {/* put the wording to the right  */}
-      <div className={styles.sepImgWord}>
-        <img className={styles.manPic} src="model.jpg" alt="model" />
-        <div className={styles.text}>
-          <h1 className={styles.title}>Grace Shopper</h1>
-          <h4>Come get your real fake cloths TODAY!!!!</h4>
-        
-          <button
-            href="#_"
-            class="relative inline-flex items-center px-8 py-1 overflow-hidden text-lg font-medium text-white border-2 border-black rounded-full hover:text-white group hover:bg-gray-50 mt-14"
-            onClick={() => {
-              nav("/products");
-            }}
-          >
-            <span class="absolute left-0 block w-full h-0 transition-all bg-black opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
-            <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
-              <svg
-                class="w-5 h-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M14 5l7 7m0 0l-7 7m7-7H3"
-                ></path>
-              </svg>
-            </span>
-            <span class="relative">Shop Now</span>
-          </button>
+      <div className="flex justify-center flex-row m-20 bg-black m-12 items-center p-6">
+        <div className="flex flex-col items-center  ">
+          <img
+            className="flex items-center justify-center relative rounded-full h-24 "
+            src={wilson}
+            alt="model"
+          />
+          <p className="text-white text-3xl justify-center mr-4">Wilson Chen</p>
         </div>
+        <div className="flex flex-col items-center gap-x-8">
+          <img
+            className="flex items-center justify-center relative rounded-full h-24 "
+            src={ferni}
+            alt="model"
+          />
+          <p className="text-white text-3xl justify-center mr-4">
+            Fernando Reyes
+          </p>
+        </div>
+        <div className="flex flex-col items-center gap-x-8">
+          <img
+            className="flex items-center justify-center relative rounded-full h-24 "
+            src={ugo}
+            alt="model"
+          />
+          <p className="text-white text-3xl justify-center">Ugonna Duru</p>
+        </div>
+      </div>
+      <div className={styles.text}>
+        <h1 className={styles.title}>Real Fake Clothes</h1>
+        <h4 className="text-customyellow">2209 Capstone Project</h4>
+
+        <button
+          href="#_"
+          class="relative inline-flex items-center px-8 py-1 overflow-hidden text-lg font-medium text-white border-2 border-black rounded-full hover:text-white group hover:bg-gray-50 mt-14"
+          onClick={() => {
+            nav("/products");
+          }}
+        >
+          <span class="absolute left-0 block w-full h-0 transition-all bg-black opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+          <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+            <svg
+              class="w-5 h-5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              ></path>
+            </svg>
+          </span>
+          <span class="relative">Shop Now</span>
+        </button>
       </div>
     </div>
   );
